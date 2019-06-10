f098f44759011cf5183de2be868a2acc87276941
Jacob Shaw <jds98929@uga.edu>
2019-06-10 16:07:37 -0400

    testing w/o socket.io

--- chat-express/index.js
+++ chat-express/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const socket = require('socket.io')(server, { origins: '*:*'});
+// const socket = require('socket.io')(server, { origins: '*:*'});
+
 // const cors = require('cors');
 
 let usernames = new Map();
@@ -28,66 +29,66 @@ const server = app.listen(3001, () => {
     console.log("Listening to requests on port 3001");
 }) 
 
-const io = socket(server);
-io.on('connection', function(socket){
-    connectCount++;
-    console.log('made socket connection', socket.id);
-    console.log('connection count: ' + connectCount);
-    socket.on('chat', function(data){
-        io.sockets.emit('chat', data);
-        console.log('received listener message:', data.message);
-        usernames.forEach(val => {
-            console.log(val);
-        })
-    });
+// const io = socket(server);
+// io.on('connection', function(socket){
+//     connectCount++;
+//     console.log('made socket connection', socket.id);
+//     console.log('connection count: ' + connectCount);
+//     socket.on('chat', function(data){
+//         io.sockets.emit('chat', data);
+//         console.log('received listener message:', data.message);
+//         usernames.forEach(val => {
+//             console.log(val);
+//         })
+//     });
 
-    socket.on('speaker chat', function(data){
-        io.sockets.emit('speaker chat', data);
-        console.log('received speaker message:', data.message);
-    });
+//     socket.on('speaker chat', function(data){
+//         io.sockets.emit('speaker chat', data);
+//         console.log('received speaker message:', data.message);
+//     });
 
-    socket.on('check speaker', function(){
-        speaker ? socket.emit('speaker status', true) : socket.emit('speaker status', false);
-    });
+//     socket.on('check speaker', function(){
+//         speaker ? socket.emit('speaker status', true) : socket.emit('speaker status', false);
+//     });
 
-    socket.on('check count', function(){
-        console.log('count', connectCount);
-        socket.emit('count', connectCount);
-    })
+//     socket.on('check count', function(){
+//         console.log('count', connectCount);
+//         socket.emit('count', connectCount);
+//     })
 
-    socket.on('connect', function(){
-        socket.emit('count', connectCount);
-    })
+//     socket.on('connect', function(){
+//         socket.emit('count', connectCount);
+//     })
 
-    socket.on('disconnect', function(){
-        connectCount--;
-        speaker = null;
-        usernames.delete(socket.id);
-        console.log('disconnected');
-    })
+//     socket.on('disconnect', function(){
+//         connectCount--;
+//         speaker = null;
+//         usernames.delete(socket.id);
+//         console.log('disconnected');
+//     })
 
-    socket.on('handle', function(data){
-        console.log('Handle: ' + data.username);
-        console.log('Role: ' + data.role);
-        let alreadyInUse = false;
-        for (const v of usernames.values()) {
-            if (v === data.username.trim()){
-                alreadyInUse = true;
-            } 
-        }
-        usernames && !alreadyInUse && usernames.set(socket.id, data.username.trim());
-        console.log(alreadyInUse + ' ' + usernames.values());
-        if (alreadyInUse) {
-            socket.emit('handle', {reply: 'taken'});
-        } else {
-            if (connectCount === 2) {                
-                if (data.role === 'speak'){
-                    speaker = socket.id;
-                } 
-                socket.emit('handle', {reply: 'success'});
-            } else{
-                socket.emit('handle', {reply: 'wait'});
-            }
-        }
-    });
-});
+//     socket.on('handle', function(data){
+//         console.log('Handle: ' + data.username);
+//         console.log('Role: ' + data.role);
+//         let alreadyInUse = false;
+//         for (const v of usernames.values()) {
+//             if (v === data.username.trim()){
+//                 alreadyInUse = true;
+//             } 
+//         }
+//         usernames && !alreadyInUse && usernames.set(socket.id, data.username.trim());
+//         console.log(alreadyInUse + ' ' + usernames.values());
+//         if (alreadyInUse) {
+//             socket.emit('handle', {reply: 'taken'});
+//         } else {
+//             if (connectCount === 2) {                
+//                 if (data.role === 'speak'){
+//                     speaker = socket.id;
+//                 } 
+//                 socket.emit('handle', {reply: 'success'});
+//             } else{
+//                 socket.emit('handle', {reply: 'wait'});
+//             }
+//         }
+//     });
+// });
